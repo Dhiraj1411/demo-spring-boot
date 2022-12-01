6850b5772a75db616692c2de5d0887b6f57a643d
Dhiraj Narayan Singh <dhirajsingh@Dhirajs-Air.hitronhub.home>
2022-11-30 22:52:21 -0700

    updated the test file

--- src/test/java/com/assessment/rbc/controller/StockTickerControllerTest.java
+++ src/test/java/com/assessment/rbc/controller/StockTickerControllerTest.java
@@ -1,4 +1,20 @@
+package com.assessment.rbc.controller;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StockTickerControllerTest {
-  
+
+    @Test
+    void getStockes() {
+    }
+
+    @Test
+    void bulkUpload() {
+    }
+
+    @Test
+    void save() {
+    }
 }
--- src/test/java/com/assessment/rbc/service/StockServiceImplTest.java
+++ src/test/java/com/assessment/rbc/service/StockServiceImplTest.java
@@ -1,4 +1,20 @@
+package com.assessment.rbc.service;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StockServiceImplTest {
-  
+
+    @Test
+    void saveStock() {
+    }
+
+    @Test
+    void bulkUpload() {
+    }
+
+    @Test
+    void getStocks() {
+    }
 }
