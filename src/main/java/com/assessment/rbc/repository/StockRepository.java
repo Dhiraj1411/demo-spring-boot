93a4fd8206eb31ee9e5c752bf673a27466ca03f9
Dhiraj Narayan Singh <dhirajsingh@Dhirajs-Air.hitronhub.home>
2022-12-17 22:55:53 -0700

    Modified properties file

--- src/main/java/com/assessment/rbc/repository/StockRepository.java
+++ src/main/java/com/assessment/rbc/repository/StockRepository.java
@@ -1,6 +1,7 @@
 package com.assessment.rbc.repository;
 
 import com.assessment.rbc.models.StockEntity;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
